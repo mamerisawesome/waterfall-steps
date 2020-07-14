208a35f5d222995c44bfc396c0e50f20289f2c03
mamerisawesome <atmalmer23@gmail.com>
2020-07-14 20:00:32 +0800

    Make sure to ignore summation if one input value is undefined

--- src/pages/Stepper/StepperPage2.tsx
+++ src/pages/Stepper/StepperPage2.tsx
@@ -20,16 +20,28 @@ type Page2Props = (
   | void
 );
 
-const getInputsSummation = (data: number[]) => {
-  return data.reduce((acc: number, val: number): number => (
-    acc + val
+const getInputsSummation = (data: number[]|undefined[]) => {
+  let areNumbers = true;
+  for (let idx = 0; idx < data.length; idx += 1) {
+    if (!data[idx]) {
+      areNumbers = false;
+      break;
+    }
+  }
+
+  if (!areNumbers) {
+    return 0;
+  }
+
+  return (data as number[]).reduce((acc: number, val: number|undefined): number => (
+    val ? (acc + val) : acc
   ), 0);
 };
 
 const Page2Component: Page2Props = ({ className }) => {
   const { step, dispatch } = useContext(StepContext);
   const [isValid, setIsValid] = useState<Boolean>(false);
-  const [inputVals, setInputVals] = useState<number[]>([]);
+  const [inputVals, setInputVals] = useState<number[]|undefined[]>([]);
 
   const handleGetSummation = useCallback(() => {
     setIsValid(getInputsSummation(inputVals) === 100);
@@ -53,6 +65,9 @@ const Page2Component: Page2Props = ({ className }) => {
     return (e: ChangeEvent<HTMLInputElement>): void => {
       const tempVals = inputVals.slice();
       tempVals[id] = Number(e.target.value);
+      if (e.target.value === "") {
+        tempVals[id] = undefined;
+      }
       setInputVals(tempVals);
     };
   }
